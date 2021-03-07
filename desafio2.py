7f08cda685a945ad0a76a0062313bda6b870671d
Gustavo Bizo Jardim <69866294+gbiz0@users.noreply.github.com>
2021-03-07 09:17:16 -0300

    Delete desafio2.py

--- desafio2.py
+++
@@ -1,5 +0,0 @@
-dia = input ('Digite o dia do seu aniversario:')
-mes = input ('Digite o mês do seu aniversario:')
-ano = input ('Digite o ano que você nasceu:')
-
-print ('Você nasceu no dia', dia, 'de' , mes, 'no ano de' , ano,)
